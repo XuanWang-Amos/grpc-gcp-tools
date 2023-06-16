8f3945bd34bacd7af5e13ee114eb29695174c142
Xuan Wang <xuanwn@google.com>
2023-06-16 19:39:55 +0000

    Fix bug

--- observability/test/o11y_tests_manager.py
+++ observability/test/o11y_tests_manager.py
@@ -239,7 +239,7 @@ class TestManager:
 
     def add_lang_jobs(self, lang: SupportedLang) -> None:
         for test_case in ObservabilityTestCase:
-            if test_case in DISABLED_OBSERVABILITY_TEST_CASE.get(lang.toEnum, default=[]):
+            if test_case in DISABLED_OBSERVABILITY_TEST_CASE.get(lang.toEnum, []):
                 continue
             queue_key = '%s:%d' % (lang, self.get_next_lang_shard_num(lang))
             self.add_job_to_job_queue(queue_key, lang, lang, test_case)
