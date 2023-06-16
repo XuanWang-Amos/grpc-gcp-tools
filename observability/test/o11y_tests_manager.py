2d12e98e0fd498d16e3dc771f5de8c7fcee177bf
Xuan Wang <xuanwn@google.com>
2023-06-16 19:32:26 +0000

    Fix bug

--- observability/test/o11y_tests_manager.py
+++ observability/test/o11y_tests_manager.py
@@ -52,7 +52,7 @@ INTEROP_TEST_CASES = [
 DISABLED_OBSERVABILITY_TEST_CASE = {
     # Logging is not implemented in Python yet.
     # TODO(xuanwx): Enabled those tests after logging is implemented.
-    SupportedLangEnum.PYTHON: set(
+    SupportedLangEnum.PYTHON: [
         ObservabilityTestCase.TEST_LOGGING_BASIC,
         ObservabilityTestCase.TEST_STREAMING,
         ObservabilityTestCase.TEST_CONFIGS_LOGGING_SERVICE_FILTER,
@@ -61,8 +61,8 @@ DISABLED_OBSERVABILITY_TEST_CASE = {
         ObservabilityTestCase.TEST_CONFIGS_LOGGING_METADATA_LIMIT,
         ObservabilityTestCase.TEST_CONFIGS_LOGGING_PAYLOAD_LIMIT,
         ObservabilityTestCase.TEST_CONFIGS_CUSTOM_LABELS,
-        ObservabilityTestCase.TEST_LOGGING_CONNECT_TRACE
-    ),
+        ObservabilityTestCase.TEST_LOGGING_CONNECT_TRACE,
+    ],
 }
 
 NUM_INTEROP_QUEUES = 2
@@ -239,7 +239,7 @@ class TestManager:
 
     def add_lang_jobs(self, lang: SupportedLang) -> None:
         for test_case in ObservabilityTestCase:
-            if test_case in DISABLED_OBSERVABILITY_TEST_CASE.get(lang.toEnum, default=set()):
+            if test_case in DISABLED_OBSERVABILITY_TEST_CASE.get(lang.toEnum, default=[]):
                 continue
             queue_key = '%s:%d' % (lang, self.get_next_lang_shard_num(lang))
             self.add_job_to_job_queue(queue_key, lang, lang, test_case)
