49d3e657188e705465bc69a1bc61ab9794e5fa75
Xuan Wang <xuanwn@google.com>
2023-06-27 22:41:31 +0000

    Add interop test support for other langs

--- observability/test/o11y_tests_manager.py
+++ observability/test/o11y_tests_manager.py
@@ -34,14 +34,18 @@ import traceback
 from typing import Dict, List
 
 INTEROP_COMBINATIONS = [
-    { 'server_lang': SupportedLangEnum.JAVA, 'client_lang': SupportedLangEnum.GO   },
-    { 'server_lang': SupportedLangEnum.JAVA, 'client_lang': SupportedLangEnum.CPP  },
-    { 'server_lang': SupportedLangEnum.GO,   'client_lang': SupportedLangEnum.JAVA },
-    { 'server_lang': SupportedLangEnum.GO,   'client_lang': SupportedLangEnum.CPP  },
-    { 'server_lang': SupportedLangEnum.CPP,  'client_lang': SupportedLangEnum.GO   },
-    { 'server_lang': SupportedLangEnum.CPP,  'client_lang': SupportedLangEnum.JAVA },
-    { 'server_lang': SupportedLangEnum.CPP,  'client_lang': SupportedLangEnum.PYTHON },
-    { 'server_lang': SupportedLangEnum.PYTHON,  'client_lang': SupportedLangEnum.CPP },
+    { 'server_lang': SupportedLangEnum.JAVA, 'client_lang': SupportedLangEnum.GO      },
+    { 'server_lang': SupportedLangEnum.JAVA, 'client_lang': SupportedLangEnum.CPP     },
+    { 'server_lang': SupportedLangEnum.JAVA, 'client_lang': SupportedLangEnum.PYTHON  },
+    { 'server_lang': SupportedLangEnum.GO,   'client_lang': SupportedLangEnum.JAVA    },
+    { 'server_lang': SupportedLangEnum.GO,   'client_lang': SupportedLangEnum.CPP     },
+    { 'server_lang': SupportedLangEnum.GO,   'client_lang': SupportedLangEnum.PYTHON  },
+    { 'server_lang': SupportedLangEnum.CPP,  'client_lang': SupportedLangEnum.GO      },
+    { 'server_lang': SupportedLangEnum.CPP,  'client_lang': SupportedLangEnum.JAVA    },
+    { 'server_lang': SupportedLangEnum.CPP,  'client_lang': SupportedLangEnum.PYTHON  },
+    { 'server_lang': SupportedLangEnum.PYTHON,  'client_lang': SupportedLangEnum.GO   },
+    { 'server_lang': SupportedLangEnum.PYTHON,  'client_lang': SupportedLangEnum.CPP  },
+    { 'server_lang': SupportedLangEnum.PYTHON,  'client_lang': SupportedLangEnum.JAVA },
 ]
 
 INTEROP_TEST_CASES = [
